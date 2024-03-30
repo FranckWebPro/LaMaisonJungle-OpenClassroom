e26e8dddeb71fdcc78c8a7245f92db8603b2055c
Franck <contact@franckwebpro.com>
2024-03-30 10:33:16 +0100

    initial commit and ordering the repo

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Banner from './Banner'
+import Cart from './Cart'
+
+function App() {
+    return (
+      <>
+      <Banner />
+      <Cart/>
+      </>
+    ) 
+}
+
+export default App
