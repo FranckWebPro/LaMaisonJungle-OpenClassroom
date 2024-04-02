2651314c73071a24515a4068eea7c5e1452011c6
Franck <contact@franckwebpro.com>
2024-04-02 21:46:38 +0200

    finalisation LMJ

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,31 @@
 import Banner from './Banner'
 import logo from '../assets/logo.png'
-// import Cart from './Cart'
-import ShoppingList from './ShoppingList'
+import Cart from './Cart'
 import Footer from './Footer'
+import ShoppingList from './ShoppingList'
+import '../styles/Layout.css'
+import { useState, useEffect } from 'react'
 
 function App() {
+	const savedCart = localStorage.getItem('cart')
+	const [cart, updateCart] = useState(savedCart ? JSON.parse(savedCart) : [])
+	const [activeCategory, setActiveCategory] = useState('')
+
+	useEffect(() => {
+		localStorage.setItem('cart', JSON.stringify(cart))
+	}, [cart])
+
 	return (
 		<div>
 			<Banner>
 				<img src={logo} alt='La maison jungle' className='lmj-logo' />
 				<h1 className='lmj-title'>La maison jungle</h1>
 			</Banner>
-			{/* <Cart /> */}
-			<ShoppingList />
-			<Footer/>
+			<div className='lmj-layout-inner'>
+				<Cart cart={cart} updateCart={updateCart}/>				
+				<ShoppingList cart={cart} updateCart={updateCart} activeCategory={activeCategory} setActiveCategory={setActiveCategory}/>
+			</div>
+			<Footer />
 		</div>
 	)
 }
